80fe49cdbed22da35a5499a35e640ee3ebe4debd
Andrew Pasquale <andrew@elytra.net>
2011-09-12 11:48:17 -0400

    Initial commit of unbound11 theme.

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='plonetheme.unbound11',
+      version=version,
+      description="Diazo theme for Unbound11",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Andrew Pasquale',
+      author_email='andrew@elytra.net',
+      url='https://a-pasquale@github.com/a-pasquale/plonetheme.unbound11.git',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
